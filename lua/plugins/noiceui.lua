ebaea923a4940df89bbd590c7821d6e27a0aea6b
Vincenzo Fehring <vinfehring@proton.me>
2024-01-27 01:25:57 -0600

    Added a noicer ui for command palette

---
+++ lua/plugins/noiceui.lua
@@ -0,0 +1,20 @@
+return {
+  -- noicer ui
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    config = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.stylize_markdown"] = true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+      },
+    },
+  },
+}
