b3955cd9f53cc68155c5bd13af69661b798bb83c
Vincenzo Fehring <vinfehring@proton.me>
2024-01-27 00:09:00 -0600

    Configure editor and added missing LSP servers

--- lua/user/plugins/lsp/servers.lua
+++ lua/user/plugins/lsp/servers.lua
@@ -1,25 +1,25 @@
 ---@type lspconfig.options
 local servers = {
+  angularls = {},
+  arduino_language_server = {},
+  astro = {},
   bashls = {},
   clangd = {},
-  cssls = {},
-  tsserver = {},
+  cssls = {}
+  denols = {},
+  docker_compose_language_service = {},
+  dockerls = {},
+  gopls = {},
+  graphql = {},
   html = {},
   jsonls = {},
+  lua_ls = {},
+  prismals = {},
   pyright = {},
-  yamlls = {},
-  sumneko_lua = {
-    settings = {
-      Lua = {
-        workspace = {
-          checkThirdParty = false,
-        },
-        completion = {
-          callSnippet = "Replace",
-        },
-      },
-    },
-  },
+  rust_analyzer = {},
+  tailwindcss = {},
+  tsserver = {},
+  vimls = {},
 }
 
 return servers
