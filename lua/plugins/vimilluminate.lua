1729670b1a73485ac1b5afaaa99a0a1d798cc363
Vincenzo Fehring <vinfehring@proton.me>
2024-01-27 13:08:09 -0600

    Fixed vimilluminate and added lazygit

---
+++ lua/plugins/vimilluminate.lua
@@ -0,0 +1,26 @@
+return {
+  -- references
+  {
+    "RRethy/vim-illuminate",
+    event = "BufReadPost",
+    config = function()
+      require("illuminate").configure({ delay = 200 })
+    end,
+    keys = {
+      {
+        "]]",
+        function()
+          require("illuminate").goto_next_reference(false)
+        end,
+        desc = "Next Reference",
+      },
+      {
+        "[[",
+        function()
+          require("illuminate").goto_prev_reference(false)
+        end,
+        desc = "Prev Reference",
+      },
+    },
+  },
+}
