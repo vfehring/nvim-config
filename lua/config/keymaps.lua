095bb507e25777c08c605afa345d61e2a9171074
Vincenzo Fehring <vinfehring@proton.me>
2024-01-27 18:41:42 -0600

    Remove ctrl+s to save

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -44,10 +44,6 @@ vim.keymap.set("i", ",", ",<c-g>u")
 vim.keymap.set("i", ".", ".<c-g>u")
 vim.keymap.set("i", ";", ";<c-g>u")
 
--- save in insert mode
-vim.keymap.set("i", "<C-s>", "<cmd>:w<cr><esc>")
-vim.keymap.set("n", "<C-s>", "<cmd>:w<cr><esc>")
-
 -- better indenting
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
